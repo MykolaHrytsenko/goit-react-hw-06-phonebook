378ff9eec8ac276e506416362119390440eb63a3
Mykola_Hrytsenko <94016020+MykolaHrytsenko@users.noreply.github.com>
2022-09-05 16:06:09 +0300

    save (not working)

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItem, getItems, getFilter } from '../../redux/contactsSlice';
 import css from './ContactList.module.css';
 
-export const ContactList = ({ contacts, onDeleteContact }) => (
-  <div>
-    <ul>
-      {contacts.map(({ id, name, number }) => (
-        <li key={id}>
-          {`${name}: ${number}`}
-          <button
-            className={css.btn}
-            data-id={id}
-            onClick={() => onDeleteContact(id)}
-          >
-            Delete
-          </button>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+export const ContactList = () => {
+  const contacts = useSelector(getItems);
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
+
+  const getContactsFilter = () => {
+    const normalizedFilter = filter.toLocaleLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizedFilter)
+    );
+  };
+  const contactsFilter = getContactsFilter();
+
+  return (
+    <div>
+      <ul>
+        {contactsFilter.map(({ id, name, number }) => (
+          <li key={id}>
+            {`${name}: ${number}`}
+            <button
+              className={css.btn}
+              type="button"
+              data-id={id}
+              onClick={() => dispatch(removeItem(id))}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
